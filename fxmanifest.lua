d8a43e8d7143a580515aca9494f727d885ff4846
ZEN Development <127617452+ZEN-Development1@users.noreply.github.com>
2023-03-11 11:47:22 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Zen ESX Discord Logs'
+author 'ZEN DEV'
+
+server_script 'server.lua'
+client_script 'client.lua'
+
+shared_script {
+    'config.lua'
+}
