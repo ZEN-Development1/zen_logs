03acddbfdc2d708a2bf9fd5f7608e7032aac660d
ZEN Development <127617452+ZEN-Development1@users.noreply.github.com>
2023-03-11 11:47:30 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,46 @@
+Config = {}
+
+-- Set to true to enable logging of gang turf captures
+Config.LogGangCapture = true
+-- Set the webhook URL to use for gang turf capture logging
+Config.GangCaptureWebhook = "https://discordapp.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxx"
+
+-- Set to true to enable logging of completed turf wars
+Config.LogTurfWarEnd = true
+-- Set the webhook URL to use for completed turf war logging
+Config.TurfWarEndWebhook = "https://discordapp.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxx"
+
+-- Set to true to enable logging of admin kicks
+Config.LogAdminKick = true
+-- Set the webhook URL to use for admin kick logging
+Config.AdminKickWebhook = "https://discordapp.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxx"
+
+-- Set to true to enable logging of player screenshots
+Config.LogPlayerScreenshot = true
+-- Set the webhook URL to use for player screenshot logging
+Config.PlayerScreenshotWebhook = "https://discordapp.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxx"
+
+-- Set to true to enable logging of player revives
+Config.LogPlayerRevive = true
+-- Set the webhook URL to use for player revive logging
+Config.PlayerReviveWebhook = "https://discordapp.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxx"
+
+-- Set to true to enable logging of player money transfers
+Config.LogPlayerMoneyTransfer = true
+-- Set the webhook URL to use for player money transfer logging
+Config.PlayerMoneyTransferWebhook = "https://discordapp.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxx"
+
+-- Set to true to enable logging of player deaths
+Config.LogPlayerDeath = true
+-- Set the webhook URL to use for player death logging
+Config.PlayerDeathWebhook = "https://discordapp.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxx"
+
+-- Set to true to enable logging of player commands
+Config.LogPlayerCommand = true
+-- Set the webhook URL to use for player command logging
+Config.PlayerCommandWebhook = "https://discordapp.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxx"
+
+-- Set to true to enable logging of inventory clearances by staff
+Config.LogInventoryClearance = true
+-- Set the webhook URL to use for inventory clearance logging
+Config.InventoryClearanceWebhook = "https://discordapp.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxx"
